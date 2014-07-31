f531a297adbf42b58caa25e6d661a04719a70ef7
Zeb DeOs <zebdeos@coquilletribe.org>
2014-07-31 16:30:38 -0700

    Add copyright blurb for lesson 7 code from lesson

--- python1/lesson7/code-from-lesson/personlist.py
+++ python1/lesson7/code-from-lesson/personlist.py
@@ -1,4 +1,17 @@
 #!/usr/local/bin/python3
+
+# Code from O'Reilly School of Technology Course:
+#   Python 1: Beginning Python
+#     Lesson 7: String Formatting
+# Entered as per the lesson text instructions.
+
+# From the lesson text:
+#   Copyright © 1998-2014 O'Reilly Media, Inc.
+#   This work is licensed under a Creative Commons
+#   Attribution-ShareAlike 3.0 Unported License.
+#   See http://creativecommons.org/licenses/by-sa/3.0/legalcode for more
+#   information. 
+
 """Produce a listing of people's names, ages, and weights."""
 data = [
         ("Steve", 59, 202),
