552ea573d29613ca409eea21eb8b2ca664a48972
Zeb DeOs <zebdeos@bayprogrammer.com>
2015-06-02 16:34:36 -0700

    Simplify triangle.js implementation

--- js/triangle.js
+++ js/triangle.js
@@ -3,8 +3,6 @@
 // Create a triangle out of octothorpes
 // (exercise from Eloquent JavaScript, 2nd ed.; ch. 2, p. 37)
 
-var line = "#"
-for (var len = 0; len < 7; len++) {
+for (var line = "#"; line.length < 8; line += "#") {
   console.log(line);
-  line += "#";
 }
