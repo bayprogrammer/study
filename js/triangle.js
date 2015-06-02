39ff8d9a5e2e63cec95653f25b2bccb2f3e3bdf3
Zeb DeOs <zebdeos@bayprogrammer.com>
2015-06-02 16:28:08 -0700

    Fix off-by-one in triangle.js script

--- js/triangle.js
+++ js/triangle.js
@@ -3,8 +3,8 @@
 // Create a triangle out of octothorpes
 // (exercise from Eloquent JavaScript, 2nd ed.; ch. 2, p. 37)
 
-line = "#"
-for (var len = 0; len < 8; len++) {
+var line = "#"
+for (var len = 0; len < 7; len++) {
   console.log(line);
   line += "#";
 }
