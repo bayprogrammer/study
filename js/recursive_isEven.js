799c9cd493d42595def18b523486f6caf799c9f8
Zeb DeOs <zebdeos@bayprogrammer.com>
2015-06-08 17:31:23 -0700

    JavaScript exercise: recursive func. to check even num

---
+++ js/recursive_isEven.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+// Define function to determine if number is even or odd;
+// rather than using Boolean(n % 2), we must use recursion;
+// make it function with negative as well as positive input.
+// (exercise from Eloquent JavaScript, 2nd ed.; ch. 3, p. 56)
+
+// To use in node:
+// var isEven = require('./recursive_isEven.js');
+// isEven.isEven(56) // => true
+// isEven.isEven(23) // => false
+
+function isEven(n){
+  n = Math.abs(n);
+  switch(n) {
+    case 0:
+      // returning value immediately breaks the switch eval; no need
+      // for explicit break (would never be reached)
+      return true
+    case 1:
+      return false
+    default:
+      return isEven(n-2)
+  }
+}
+
+exports.isEven = isEven
