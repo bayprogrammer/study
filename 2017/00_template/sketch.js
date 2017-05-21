18493dd251836fde770ab43b894e11d8755b286a
Zeb DeOs <zebdeos@bayprogrammer.com>
2017-05-20 23:13:49 -0700

    Vendor latest p5.js & bootstrap 2017 sketchbook

---
+++ 2017/00_template/sketch.js
@@ -0,0 +1,9 @@
+/** sketch.js ****************************************************************/
+
+function setup() {
+  bayprogrammer.p5.setupCanvas(50, 50, 80, 100);
+}
+
+function draw() {
+  rect(200, 200, 150, 100);
+}
---
+++ 2017/00_template/utils.js
@@ -0,0 +1,94 @@
+/** utils.js *****************************************************************/
+
+function ns(ns, obj, root) {
+  root = findGlobal(root);
+
+  var target;
+  var parent = root;
+  var segments = ns.split('.');
+
+  for (var i = 0; i < segments.length; i++) {
+    if (i == segments.length - 1) {
+      target = segments[i];
+    } else {
+      parent[segments[i]] = parent[segments[i]] || {};
+      parent = parent[segments[i]];
+    }
+  }
+
+  parent[target] = obj;
+}
+
+function findGlobal(root) {
+  if (!root) {
+    if (window !== undefined) {
+      root = window;
+    } else if (global !== undefined) {
+      root = global;
+    } else {
+      throw 'Unable to find global object!';
+    }
+  }
+
+  return root;
+}
+
+ns('bayprogrammer.p5.setupCanvas', (function() {
+
+  return function (margin, ...canvas_bg) {
+    createCanvas(windowWidth - margin, windowHeight - margin);
+    background(...canvas_bg);
+  };
+
+})());
+
+/** utils.js (internal) ******************************************************/
+
+ns('bayprogrammer.p5.setupCanvas', (function() {
+
+  var _w;
+  var _h;
+
+  var _setupStyle = function(margin, canvas_bg) {
+    background(...canvas_bg);
+
+    var page_bg = [];
+    page_bg[0] = canvas_bg[0] + 20;
+    page_bg[1] = canvas_bg[1] + 20;
+    page_bg[2] = canvas_bg[2] + 20;
+
+    var canvasEle = document.getElementsByTagName('canvas')[0];
+    canvasEle.style.display = 'block';
+    canvasEle.style.padding = '0';
+    canvasEle.style.margin = '0';
+    canvasEle.style.marginLeft = 'auto';
+    canvasEle.style.marginRight = 'auto';
+    canvasEle.style.marginTop = `${margin/2}px`;
+
+    var htmlEle = document.getElementsByTagName('html')[0];
+    htmlEle.style.backgroundColor = (
+      `rgb(${page_bg[0]}, ${page_bg[1]}, ${page_bg[2]})`);
+    htmlEle.style.margin = '0';
+    htmlEle.style.padding = '0';
+    htmlEle.style.width = '100%';
+
+    var bodyEle = document.getElementsByTagName('body')[0];
+    bodyEle.style.margin = '0';
+    bodyEle.style.padding = '0';
+    bodyEle.style.width = '100%';
+  };
+
+  return function(margin, ...canvas_bg) {
+    _w = () => windowWidth - margin;
+    _h = () => windowHeight - margin;
+
+    createCanvas(_w(), _h());
+    _setupStyle(margin, canvas_bg);
+
+    findGlobal().windowResized = () => {
+      resizeCanvas(_w(), _h());
+      _setupStyle(margin, canvas_bg);
+    };
+  };
+
+})());
