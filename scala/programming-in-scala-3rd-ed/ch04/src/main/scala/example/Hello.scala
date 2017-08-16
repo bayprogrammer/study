6a76075bb146b5bb0ec52bed4c58941adad8f57c
Zeb DeOs <zebdeos@bayprogrammer.com>
2017-08-15 20:08:39 -0700

    Prog in Scala: Ch 04: initial ChecksumAccumulator

--- scala/programming-in-scala-3rd-ed/ch04/src/main/scala/example/Hello.scala
+++ scala/programming-in-scala-3rd-ed/ch04/src/main/scala/example/Hello.scala
@@ -2,6 +2,11 @@ package example
 
 object Hello extends Greeting with App {
   println(greeting)
+
+  val csa = new ChecksumAccumulator
+  csa.add(0x1)
+  csa.add(0x1)
+  println(csa.checksum())
 }
 
 trait Greeting {
