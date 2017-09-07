33c9398f79c7066397aa4b8e1972ee777d3d7abc
Zeb DeOs <zebdeos@bayprogrammer.com>
2017-09-06 20:48:48 -0700

    Complete The Rust Programming Language, Ch 04

--- rust/the-rust-programming-language-2ed/projects/misc/src/main.rs
+++ rust/the-rust-programming-language-2ed/projects/misc/src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}!", number);
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("{}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
     }
 
-    println!("LIFTOFF!!!");
+    &s[..]
 }
