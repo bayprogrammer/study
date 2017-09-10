c423095e2300b61c914f7e087a9e2fa655ca9bee
Zeb DeOs <zebdeos@bayprogrammer.com>
2017-09-09 19:36:40 -0700

    Convert game grid & stand-alone functions to struct type with methods

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,52 @@
 const GRID_STRIDE: usize = 10;
-const GRID_ROWS: usize   = 10;
-const GRID_SIZE: usize   = GRID_ROWS * GRID_STRIDE;
+const GRID_ROWS:   usize = 10;
+const GRID_SIZE:   usize = GRID_ROWS * GRID_STRIDE;
 
 fn main() {
-    let mut game_grid = new_game_grid();
+    let mut game_grid = GameGrid::new();
 
-    print_game_grid(&mut game_grid);
+    game_grid.display();
 
     println!("");
 
-    tick_game_grid(&mut game_grid);
-    print_game_grid(&mut game_grid);
+    game_grid.tick();
+    game_grid.display();
 }
 
-// TODO(zmd): can we make a custom array type and implement the following as
-//     methods on it?
-
-fn new_game_grid() -> [bool; GRID_SIZE] {
-    [true; GRID_SIZE]
+struct GameGrid {
+    grid: [bool; GRID_SIZE],
 }
 
-fn tick_game_grid(grid: &mut[bool; GRID_SIZE]) {
-    let mut i = 0;
-    for x in grid.iter_mut() {
-        if i % 3 == 0 {
-            *x = false;
+impl GameGrid {
+    fn new() -> GameGrid {
+        GameGrid {
+            grid: [true; GRID_SIZE]
         }
-        i += 1;
     }
-}
 
-fn print_game_grid(grid: &[bool; GRID_SIZE]) {
-    let mut i = 0;
-    for x in grid.iter() {
-        if *x {
-            print!("# ")
-        } else {
-            print!("  ")
+    fn tick(&mut self) {
+        let mut i = 0;
+        for x in self.grid.iter_mut() {
+            if i % 3 == 0 {
+                *x = false;
+            }
+            i += 1;
         }
+    }
 
-        i += 1;
-        if (i > 0) && (i % GRID_STRIDE == 0) {
-            print!("\n");
+    fn display(&self) {
+        let mut i = 0;
+        for x in self.grid.iter() {
+            if *x {
+                print!("# ")
+            } else {
+                print!("  ")
+            }
+
+            i += 1;
+            if (i > 0) && (i % GRID_STRIDE == 0) {
+                print!("\n");
+            }
         }
     }
 }
