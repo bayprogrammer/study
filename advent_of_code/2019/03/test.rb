a96835683eb0ebae725f5b36879a44241adb2eaa
Zeb DeOs <zebdeos@bayprogrammer.com>
2019-12-03 20:33:29 -0800

    Day 3: Part 1, WIP

--- advent_of_code/2019/03/test.rb
+++ advent_of_code/2019/03/test.rb
@@ -1,19 +1,184 @@
 #!/usr/bin/env ruby
 
-require "minitest/autorun"
+require 'minitest/autorun'
+require_relative 'wires_crossed'
+require 'set'
 
 
-class TestMeme < Minitest::Test
+class TestWiresCrossed < Minitest::Test
+
+  FrontPanel = WiresCrossed::FrontPanel
+  Wire = WiresCrossed::Wire
+
+  P = WiresCrossed::Point
+
+  L = WiresCrossed::Left
+  R = WiresCrossed::Right
+  U = WiresCrossed::Up
+  D = WiresCrossed::Down
 
   def setup
   end
 
-  def test_truth
-    assert_equal 2, 2
+  SAMPLE_DATA = [
+
+    # sample_1
+    {
+      input: "R8,U5,L5,D3\nU7,R6,D4,L4",
+      directions:     [[ R[8], U[5], L[5], D[3] ],
+                       [ U[7], R[6], D[4], L[4] ]],
+      wires: [ Wire.new( R[8], U[5], L[5], D[3] ),     # wire_1
+               Wire.new( U[7], R[6], D[4], L[4] ) ],   # wire_2
+      result: 6
+    },
+
+    {
+      input: "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83",
+      directions:     [[ R[75], D[30], R[83], U[83], L[12], D[49], R[71], U[ 7], L[72] ],
+                       [ U[62], R[66], U[55], R[34], D[71], R[55], D[58], R[83]        ]],
+      wires: [ Wire.new( R[75], D[30], R[83], U[83], L[12], D[49], R[71], U[ 7], L[72] ),
+               Wire.new( U[62], R[66], U[55], R[34], D[71], R[55], D[58], R[83]        )],
+      panel_display: %Q(
+      ).strip,
+      result: 159
+    },
+
+    {
+      input: "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+      directions:     [[ R[98], U[47], R[26], D[63], R[33], U[87], L[62], D[20], R[33], U[53], R[51] ],
+                       [ U[98], R[91], D[20], R[16], D[67], R[40], U[ 7], R[15], U[6],  R[ 7]        ]],
+      wires: [ Wire.new( R[98], U[47], R[26], D[63], R[33], U[87], L[62], D[20], R[33], U[53], R[51] ),
+               Wire.new( U[98], R[91], D[20], R[16], D[67], R[40], U[ 7], R[15], U[6],  R[ 7]        )],
+      result: 135
+    }
+
+  ]
+
+  def sample_1_intersection_distances
+    Set[11, 6]
   end
 
-  def test_lies
-    refute_equal 2, 3
+  def wire_1
+    SAMPLE_DATA[0][:wires][0]
   end
+
+  def wire_1_string
+    <<~END_OF_DISPLAY
+         +----+
+         |    |
+         |    |
+         |    |
+              |
+      o-------+
+    END_OF_DISPLAY
+  end
+
+  def wire_1_points
+    [P[0, 0], P[8, 0], P[8, -5], P[3, -5], P[3, -2]]
+  end
+
+  def wire_1_array
+    [
+      [' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '+'],
+      [' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'],
+      [' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'],
+      [' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+      ['o', '-', '-', '-', '-', '-', '-', '-', '-', '+']
+    ]
+  end
+
+  def wire_2
+    SAMPLE_DATA[0][:wires][1]
+  end
+
+  def wire_2_string
+    <<~END_OF_DISPLAY
+      +-----+
+      |     |
+      |     |
+      |     |
+      | ----+
+      |
+      |
+      o
+    END_OF_DISPLAY
+  end
+
+  def wire_2_points
+    [P[0, 0], P[0, -7], P[6, -7], P[6, -3], P[2, -3]]
+  end
+
+  def wire_2_array
+    [
+      ['+', '-', '-', '-', '-', '-', '+'],
+      ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+      ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+      ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+      ['|', ' ', '-', '-', '-', '-', '+'],
+      ['|', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['|', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['o', ' ', ' ', ' ', ' ', ' ', ' ']
+    ]
+  end
+
+  def test_point_equality
+    assert_equal(P[0, 0], P[0, 0])
+    assert_equal(P[-4, 5], P[-4, 5])
+    refute_equal(P[-4, 5], P[-5, 4])
+  end
+
+  def test_direction_equality
+    assert_equal(R[98], R[98])
+    refute_equal(R[98], L[98])
+    refute_equal(R[98], R[97])
+  end
+
+  def test_wire_equality
+    assert_equal(Wire.new(R[98], R[98]), Wire.new(R[98], R[98]))
+    refute_equal(Wire.new(R[98], R[98]), Wire.new(R[98], L[98]))
+    refute_equal(Wire.new(R[98], R[98]), Wire.new(R[98], R[97]))
+  end
+
+  def test_parse_directions
+    SAMPLE_DATA.each do |sample|
+      assert_equal(sample[:directions], FrontPanel.parse_directions(sample[:input]))
+    end
+  end
+
+  def test_wires_from
+    SAMPLE_DATA.each do |sample|
+      assert_equal(sample[:wires], FrontPanel.wires_from(sample[:directions]))
+    end
+  end
+
+  def test_wire_points
+    assert_equal(wire_1_points, wire_1.points)
+    assert_equal(wire_2_points, wire_2.points)
+  end
+
+  def test_wire_to_a
+    assert_equal(wire_1_array, wire_1.to_a)
+    assert_equal(wire_2_array, wire_2.to_a)
+  end
+
+  def test_wire_to_s
+    skip("can't convert wire to string until we have the wire's buffer")
+    assert_equal(wire_1_string, wire_1.to_s)
+    assert_equal(wire_2_string, wire_2.to_s)
+  end
+
+  # TODO(zmd): panel_display
+
+  # wire_3_display = %Q(
+  #   +-----+
+  #   |     |
+  #   |  +--X-+
+  #   |  |  | |
+  #   | -X--+ |
+  #   |  |    |
+  #   |       |
+  #   o-------+
+  # ).strip
 
 end
--- advent_of_code/2019/03/wires_crossed.rb
+++ advent_of_code/2019/03/wires_crossed.rb
@@ -1,3 +1,196 @@
 #!/usr/bin/env ruby
 
-puts "Hello, world."
+# frozen_string_literal: true
+
+
+module WiresCrossed
+
+  class FrontPanel
+
+    def self.parse_directions(input)
+      wires = input.strip.split("\n").map do |wire_string|
+        wire_string.split(',').map do |dir_string|
+          dir = dir_string[0]
+          steps = dir_string.slice(1..).to_i
+
+          case dir
+          when "L"
+            Left[steps]
+          when "R"
+            Right[steps]
+          when "U"
+            Up[steps]
+          when "D"
+            Down[steps]
+          end
+        end
+      end
+    end
+
+    def self.wires_from(dirs_collections)
+      dirs_collections.map { |directions| Wire.new(*directions) }
+    end
+
+    def initialize(*wires)
+    end
+
+  end
+
+
+  class Wire
+
+    attr_reader :directions
+    attr_reader :points
+
+    def initialize(*directions)
+      @points = points_from_directions(directions).freeze
+    end
+
+    def to_a
+      @array ||= array_from_points
+    end
+
+    def ==(other)
+      points == other.points
+    end
+
+    def to_s
+      'o'
+    end
+
+    private
+
+    def points_from_directions(directions)
+      points = []
+      cursor = [0, 0]
+      points << Point[*cursor]
+
+      directions.each do |direction|
+        case direction
+        when Left
+          x, y = cursor
+          cursor = [x - direction.steps, y]
+        when Up
+          x, y = cursor
+          cursor = [x, y - direction.steps]
+        when Right
+          x, y = cursor
+          cursor = [x + direction.steps, y]
+        when Down
+          x, y = cursor
+          cursor = [x, y + direction.steps]
+        end
+
+        points << Point[*cursor]
+      end
+
+      points
+    end
+
+
+    #
+    # [P[0, 0], P[0, -7], P[6, -7], P[6, -3], P[2, -3]]
+    #
+    #                   |
+    #                   |
+    #                   V
+    #
+    # [
+    #   ['+', '-', '-', '-', '-', '-', '+'],
+    #   ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+    #   ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+    #   ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+    #   ['|', ' ', '-', '-', '-', '-', '+'],
+    #   ['|', ' ', ' ', ' ', ' ', ' ', ' '],
+    #   ['|', ' ', ' ', ' ', ' ', ' ', ' '],
+    #   ['o', ' ', ' ', ' ', ' ', ' ', ' ']
+    # ]
+    #
+    def array_from_points
+      array = []
+
+      prev = points.first
+      last = points.last
+
+      points.each do |point|
+        x, y = point
+        # TODO(zmd) <-- LEFT OFF HERE: figure out angle, calculate char based on prev, last, and angle
+        char = point_char(point, prev, last)
+        # TODO(zmd): insert_point_into_array!(array)
+      end
+
+      # TODO(zmd): ensure any gaps in the array have been turned into ' '
+      # TODO(zmd): maybe switch to a custom data structure: Grid (which can worry about how to generate the array at the end filling in the gaps and doing any reverse magic it needs to, or else figuring out bounds ahead of time and then pre-allocating the buffer to fit; etc.)
+
+      array
+    end
+
+    def point_char(point, prev, last)
+      return 'o' if point == prev
+      # TODO(zmd): finish me!
+      '$'
+    end
+
+    def insert_point_into_array!(array)
+    end
+
+  end
+
+
+  #
+  #  .---- +x
+  #  |
+  #  |
+  # +y
+  #
+  class Point
+
+    class << self
+      alias_method :[], :new
+    end
+
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
+    def ==(other)
+      (x == other.x) && (y == other.y)
+    end
+
+  end
+
+
+  class Direction
+
+    attr_reader :steps
+
+    class << self
+      alias_method :[], :new
+    end
+
+    def initialize(steps)
+      @steps = steps
+    end
+
+    def dir
+      self.class
+    end
+
+    def ==(other)
+      (dir == other.dir) && (steps == other.steps)
+    end
+
+  end
+
+  class Left < Direction; end
+
+  class Right < Direction; end
+
+  class Up < Direction; end
+
+  class Down < Direction; end
+
+end
