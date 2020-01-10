af737f321a2b0ff0bc290e701213c083bc754f1b
Zeb DeOs <zebdeos@bayprogrammer.com>
2020-01-09 20:19:49 -0800

    How about default values?

--- harder_better_faster_stronger.rb
+++ harder_better_faster_stronger.rb
@@ -4,7 +4,7 @@ require_relative './daft.rb'
 class HarderBetterFasterStronger
   extend Daft
 
-  defn harder(how_hard: Integer) >> String do
+  defn harder(how_hard: Integer, mulitplier: [Integer, 2]) >> String do
   end
 
 end
