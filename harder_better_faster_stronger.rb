b640ee1057c30dc72a91d47ca58258b3853c420a
Zeb DeOs <zebdeos@bayprogrammer.com>
2020-01-10 15:26:06 -0800

    `if` is an enabler

--- harder_better_faster_stronger.rb
+++ harder_better_faster_stronger.rb
@@ -4,7 +4,14 @@ require_relative './daft.rb'
 class HarderBetterFasterStronger
   extend Daft
 
-  defn harder(how_hard: Integer, mulitplier: [Integer, 2]) >> String do
+  defn harder(how_hard: Integer, multiplier: [Integer, 2]) >> String do
+    iph { how_hard < 2 }.then {
+      "not hard enough"
+    }.elsiph { how_hard == 2 }.then {
+      "adequate"
+    }.else {
+      "now we're talking!"
+    }.call
   end
 
 end
