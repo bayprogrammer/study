8f62d17638f29b84f9cdd0cbc139822ae7a5c901
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-02-11 18:05:02 -0800

    Put submodules INSIDE the toplevel project module

--- taskelot.rb
+++ taskelot.rb
@@ -1,59 +1,67 @@
 #!/usr/bin/env ruby
 
-module Taskelot; end
-
-class Taskelot::App
-  def self.start()
-    new().run()
+# TODO(zmd): gemspec, etc. (proper project structure)
+# TODO(zmd): file per module
+# TODO(zmd): defcmd helper "class macro"
+module Taskelot
+  module Commandable
   end
 
-  def initialize()
-    @running = false
-  end
-
-  def run()
-    @running = true
-    while @running
-      cmd, *args = read_input()
-      dispatch_command(cmd, args)
+  class Taskelot::App
+    def self.start()
+      new().run()
     end
-  end
 
-  private
-
-  def read_input()
-    print '> '
-    if (line = gets)
-      line.split()
-    else
-      []
+    def initialize()
+      @running = false
     end
-  end
 
-  def dispatch_command(cmd, args)
-    # replace this with instead defcmd and a lookup table
-    case cmd
-    when nil
-      nothing()
-    when 'quit'
+    def run()
+      @running = true
+      while @running
+        cmd, *args = read_input()
+        dispatch_command(cmd, args)
+      end
+    end
+
+    private
+
+    def read_input()
+      print '> '
+      if (line = gets)
+        line.split()
+      else
+        []
+      end
+    end
+
+    def dispatch_command(cmd, args)
+      # replace this with instead defcmd and a lookup table
+      case cmd
+      when nil
+        nothing()
+      when 'quit'
+        quit()
+      else
+        ok()
+      end
+    end
+
+    def quit()
+      @running = false
+      puts 'Goodbye.'
+    end
+
+    def nothing()
+      puts "quit"
       quit()
-    else
-      ok()
     end
-  end
 
-  def quit()
-    @running = false
-    puts 'Goodbye.'
-  end
-
-  def nothing()
-    print "\n"
-  end
-
-  def ok()
-    puts 'OK'
+    def ok()
+      puts 'OK'
+    end
   end
 end
+
 
 Taskelot::App.start()
