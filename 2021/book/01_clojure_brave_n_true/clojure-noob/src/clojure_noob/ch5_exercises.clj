41dcc62b2d6388048a3fe55ef0b0e10c44b3d9bf
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-06-24 15:25:29 -0700

    WIP - Chapter 5

--- 2021/book/01_clojure_brave_n_true/clojure-noob/src/clojure_noob/ch5_exercises.clj
+++ 2021/book/01_clojure_brave_n_true/clojure-noob/src/clojure_noob/ch5_exercises.clj
@@ -1,17 +1,5 @@
 (ns clojure-noob.ch5-exercises)
 
-;; (def c-int (comp :intelligence :attributes))
-;; (def c-str (comp :strength :attributes))
-;; (def c-dex (comp :dexterity :attributes))
-
-;; (c-int character)
-;; (c-str character)
-;; (c-dex character)
-
-(get (partial :attributes character) :intelligence)
-(defn attr [attribute]
-  (get (partial :attributes character) attribute))
-
 ;; ===== chapter 5, exercise 1 =====
 
 (def character
@@ -33,7 +21,52 @@
 
 ;; ===== chapter 5, exercise 2 =====
 
+;;
+;; Note: I already did then once inside ch5_shippets.clj as I was reading the
+;; chapter; there I relied on reduce to do the combination, so here I'll focus
+;; on using a different technique.
+;;
+;; Probably a number of ways to implement it, but I think it will be fun to
+;; implement this time building up nested lambdas to hold the pending
+;; computation (no need to reverse that way).
+;;
+;; If only I knew how to build Clojure macros. :)
+;;
+
+(defn bind [f g]
+  (fn [& args] (f (apply g args))))
+
+(defn sq [n] (* n n))
+(def sq-inc (bind sq inc))
+(sq-inc 2)
+
+(defn my-comp-binder [current-fn fns]
+  (if (empty? fns)
+    current-fn
+    (bind current-fn (my-comp-binder (first fns) (rest fns)))))
+
 (defn my-comp [& fns]
-  ;; TODO(zmd): implement me
-  )
+  (my-comp-binder (first fns) (rest fns)))
+
+((my-comp inc *) 2 3)
+
+(def spell-slots-my-comp (my-comp int inc #(/ % 2) c-int))
+(spell-slots-my-comp character)
+
+(defn sum-varargs [& nums]
+  (loop [nums-remaining nums
+         total 0]
+    (if (empty? nums-remaining)
+      total
+      (recur (rest nums-remaining) (+ total (first nums-remaining))))))
+
+(defn sq [n] (* n n))
+
+(def sum-inc-sq (comp sq inc sum-varargs))
+(sum-inc-sq 1 2 3 4 5)
+
+(def my-sum-inc-sq (my-comp sq inc sum-varargs))
+(my-sum-inc-sq 1 2 3 4 5)
+
+;; ===== chapter 5, exercise 2 =====
 
