8e97837020e3dc5f3a042ca631390e76942fb569
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-06-26 18:50:32 -0700

    Look into Clojure's cond and case

--- 2021/junk/first.clj
+++ 2021/junk/first.clj
@@ -1,4 +1,4 @@
-;; -- threading macro? --------------------------------------------------------
+;; -- -> threading macro? -----------------------------------------------------
 
 (-> 5
     (+ 1)
@@ -12,3 +12,49 @@
     (assoc 0 "cars")
     (assoc 1 "trucks")
     (assoc 2 "planes"))  ;=> ["cars" "trucks" "planes"]
+
+;; -- cond --------------------------------------------------------------------
+
+;; https://clojure.github.io/clojure/clojure.core-api.html#clojure.core/cond
+;; https://clojuredocs.org/clojure.core/cond
+
+(cond
+  (= 0 1) :a
+  (= 1 0) :b
+  (= 1 1) :c
+  (= 0 0) :d)
+
+(cond
+  (= 0 1) :a
+  (= 1 0) :b
+  (= 'foo 'bar) :c
+  (= 'baz 'quux) :d
+  :else 'frobnicate)
+
+;; -- case --------------------------------------------------------------------
+
+;; https://clojure.github.io/clojure/clojure.core-api.html#clojure.core/case
+;; https://clojuredocs.org/clojure.core/case
+
+(defn del [arg]
+  [arg "deleted"])
+
+(defn add [arg]
+  [arg "added"])
+
+(let [action [:add "delicious sharp cheese"]]
+  (let [[cmd arg] action]
+    (case cmd
+      :del (del arg)
+      :add (add arg))))
+
+(defn dispatch-action [[cmd arg]]
+  (case cmd
+    :del (del arg)
+    :add (add arg)))
+
+(let [action [:add "super awesome robots!"]]
+  (dispatch-action action))
+
+(let [action [:del "super woke employer!"]]
+  (dispatch-action action))
