293a5fe2ed4df344223de97c261c82b0e73b6ab1
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-06-28 09:39:48 -0700

    WIP - Chapter 7 (Brave'n'True)

---
+++ 2021/book/01_clojure_brave_n_true/clojure-noob/src/clojure_noob/ch7_exercises.clj
@@ -0,0 +1,12 @@
+(ns clojure-noob.ch7-exercises)
+
+;; ===== chapter 7, exercise 1 =====
+
+;; I know how to use `list`, `quote`, `read-string`, `println`, and `eval`...
+;; but I do not really understand what this exercise is asking me to
+;; accomplish.
+
+(eval (list (read-string "println") "Zeb" "Star Trek"))
+
+;; ===== chapter 7, exercise 2 =====
+
