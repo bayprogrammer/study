317504024d48f7bc259a79dc4b2cea372105fad6
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-06-28 20:52:32 -0700

    WIP - Chapter 7 (Brave'n'True)

--- 2021/book/01_clojure_brave_n_true/clojure-noob/src/clojure_noob/ch7_exercises.clj
+++ 2021/book/01_clojure_brave_n_true/clojure-noob/src/clojure_noob/ch7_exercises.clj
@@ -10,3 +10,80 @@
 
 ;; ===== chapter 7, exercise 2 =====
 
+(def op-sym-map
+  { + '+
+   '+ '+
+    - '-
+   '- '-
+    * '*
+   '* '*
+    / '/
+   '/ '/})
+
+(def op-precedence
+  { + :weak
+   '+ :weak
+    - :weak
+   '- :weak
+    * :strong
+   '* :strong
+    / :strong
+   '/ :strong})
+
+
+
+
+
+;; TODO(zmd): grouping not working correctly yet for higher precedence
+;;   operators; we're grouping left too eagerly (we need to check the next op
+;;   first?)
+(defn group [[expr op & rst-exprs :as exprs]]
+  (cond
+    (nil? op)
+      expr
+    :else
+      (let [precedence (get op-precedence op)]
+        (case precedence
+          :weak   (group-left exprs)
+          :strong (list expr op (group-right rst-exprs))))))
+
+;;
+;; TODO(zmd): normalize ops to syms
+;;
+
+(defn group-left [[expr1 op expr2 & exprs]]
+  (cond
+    (nil? op) expr1
+    :else (group (cons (list expr1 op expr2) exprs))))
+
+(defn group-right [[expr op & exprs]]
+  (cond
+    (nil? op) expr
+    :else (list expr op (group exprs))))
+
+;; (group '(1 + 2 - 3 + 4 - 5))
+;; (group '(1 + 2 * 3 + 4 / 5))
+
+
+
+
+
+(defn infix->prefix []
+  ;; TODO(zmd): implement me
+  )
+
+;; (infix->prefix '(1 + 3 * 4 - 5))
+;; (eval (infix->prefix '(1 + 3 * 4 - 5)))
+;; (infix->prefix (list 1 + 3 * 4 - 5))
+;; (eval (infix->prefix (list 1 + 3 * 4 - 5)))
+
+;; (infix->prefix '(1 + 24 / 2 - 5))
+;; (eval (infix->prefix '(1 + 24 / 2 - 5)))
+;; (infix->prefix (list 1 + 24 / 2 - 5))
+;; (eval (infix->prefix (list 1 + 24 / 2 - 5)))
+
+;; (infix->prefix '(2 * 3 + 8 / 2))
+;; (eval (infix->prefix '(2 * 3 + 8 / 2)))
+;; (infix->prefix (list 2 * 3 + 8 / 2))
+;; (eval (infix->prefix (list 2 * 3 + 8 / 2)))
+
