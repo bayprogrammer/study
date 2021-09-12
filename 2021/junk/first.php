ec2af0cdf7461dab5da2fdc7305fff481f935650
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-09-11 18:58:09 -0700

    Start a first scratch file for PHP play

---
+++ 2021/junk/first.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+echo "Hello, world.\n";
+
+function hiThere()
+{
+  echo "Hi, there.\n";
+}
+
+hiThere();
+
+# ----------------------------------------------------------------------------
