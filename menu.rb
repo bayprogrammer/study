f3e08af4e8fe9476524d50ab1d1448b8f28ff850
Zeb DeOs <zebdeos@bayprogrammer.com>
2022-05-07 22:42:33 -0400

    Fix whatever I had in progress so it runs again

--- menu.rb
+++ menu.rb
@@ -2,18 +2,81 @@
 
 module ANSI
 
+  class EscapeCode
+
+    RESET = "\u001b[0m"
+
+    def initialize(code)
+      @code = code
+    end
+
+    def to_s
+      @code
+    end
+
+    def call(str)
+      # TODO(zmd): only add reset if it's missing (since these may be nested
+      #     together, no need to string together duplicate reset codes)
+      @code + str + RESET
+    end
+
+    alias :[] :call
+
+  end
+
+
+  # Codes that use EscapeCode can be applied to strings (things like styles and
+  # colors); those that are bare strings are meant to be added into a string or
+  # stream, but do not make sense to "apply" to a group (like moving the cursor
+  # or resetting)
+  CODES = {
+    control: {
+      :reset => "\u001b[0m",
+    },
+    style: {
+      :bold => EscapeCode.new("\u001b[1m"),
+    },
+    color: {
+      8 => {
+        :black   => EscapeCode.new("\u001b[30m"),
+        :red     => EscapeCode.new("\u001b[31m"),
+        :green   => EscapeCode.new("\u001b[32m"),
+        :yellow  => EscapeCode.new("\u001b[33m"),
+        :blue    => EscapeCode.new("\u001b[34m"),
+        :magenta => EscapeCode.new("\u001b[35m"),
+        :cyan    => EscapeCode.new("\u001b[36m"),
+        :white   => EscapeCode.new("\u001b[37m")
+      },
+      16 => {
+        # TODO(zmd): complete me
+      },
+      256 => {
+        # TODO(zmd): complete me
+      },
+      :true => ->(color) {
+        if color.is_a?(Number)
+          # TODO(zmd): generate escape code for 24 bit color represented by the
+          #     number
+        else
+          # TODO(zmd): parse string in format '#aabbcc' as hex digit
+          # TODO(zmd): use number to generate escape code for 24 bit color
+        end
+      }
+    }
+  }
+
 
   module Color
 
     BASIC_COLORS = {
-      30 => "\u001b[30m",
-      31 => "\u001b[31m",
-      32 => "\u001b[32m",
-      33 => "\u001b[33m",
-      34 => "\u001b[34m",
-      35 => "\u001b[35m",
-      36 => "\u001b[36m",
-      37 => "\u001b[37m",
+      30 => CODES[:color][8][:black],
+      31 => CODES[:color][8][:red],
+      32 => CODES[:color][8][:green],
+      33 => CODES[:color][8][:yellow],
+      34 => CODES[:color][8][:blue],
+      35 => CODES[:color][8][:magenta],
+      36 => CODES[:color][8][:cyan],
+      37 => CODES[:color][8][:white],
     }
 
     COLOR_NAMES = {
@@ -90,7 +153,7 @@ def white(str)
 end
 
 def color(color, str)
-  ANSI::Color[color] + str + reset
+  ANSI::Color[color][str]
 end
 
 def reset()
